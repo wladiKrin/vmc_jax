fixed MC sampling and calculation of covariance; results coincide with ordinary TDVP for epsilon=0

jVMC/sampler.py
@@ -270,21 +270,15 @@ class MCSampler:
 
         configs, psi = self._get_samples_mcmc(parameters, numSamples, multipleOf)
         prob_eps = self._prob_regularization(psi, self.psi_regularization)
-        prob_eps = prob_eps # / mpi.global_sum(prob_eps)
 
-        # p = jnp.conjugate(psi)/(prob_eps * mpi.global_sum(jnp.abs(psi)**2))
-        # print(mpi.global_sum(jnp.abs(psi)**2))
-        # print(mpi.global_sum(jnp.abs(psi)))
         p1 = jnp.conjugate(psi)/prob_eps
         p2 = 1/prob_eps
         Nsamples = mpi.globNumSamples
-        # print(p1[1][1])
-        # print(p2[1][1])
-        # print(mpi.global_sum(p2))
-        # print(mpi.global_sum(prob_eps)/configs.shape[1])
-        return configs, psi, p1/Nsamples, p2/Nsamples, mpi.global_sum(prob_eps)/Nsamples
+
+        return configs, psi, p1/Nsamples, p2/Nsamples
 
     def _prob_regularization(self, psi, psi_regularization):
+        ### TODO: rescale the cutoff by the maximal value of psi across all samples ###
         abs2Psi = jnp.abs(psi)**2 - psi_regularization
         mask = jnp.where(abs2Psi < 0, 0, 1)
         prob = abs2Psi * mask + psi_regularization
@@ -387,9 +381,9 @@ class MCSampler:
             newStates = vmap(updateProposer, in_axes=(0, 0, None))(newKeys[:len(carry[0])], carry[0], updateProposerArg)
 
             # Compute acceptance probabilities
-            newAccProb = jax.vmap(lambda y: self._prob_regularization(net(params, y), self.net.psi_regularization), in_axes=(0,))(newStates)
-            # print("newAccProb: ", newAccProb)
-            # print("oldProb: ", carry[1])
+            newAccProb = jax.vmap(lambda y: self._prob_regularization(net(params, y), self.psi_regularization), in_axes=(0,))(newStates)
+            # newAccProb = jax.vmap(lambda y: jnp.abs(net(params, y))**2, in_axes=(0,))(newStates) #self.net.psi_regularization
+            # newAccProb = jax.vmap(lambda y: self.mu * jnp.real(net(params, y)), in_axes=(0,))(newStates)
             P = newAccProb / carry[1]
 
             # Roll dice
@@ -419,7 +413,7 @@ class MCSampler:
         # Initialize logAccProb
         net, _ = self.net.get_sampler_net()
         self.logAccProb = global_defs.pmap_for_my_devices(
-            lambda x: jax.vmap(lambda y: self.mu * jnp.real(net(netParams, y)), in_axes=(0,))(x)
+            lambda x: jax.vmap(lambda y: self._prob_regularization(net(netParams, y), self.psi_regularization), in_axes=(0,))(x)
         )(self.states)
 
         shape = (global_defs.device_count(),) + (1,)

jVMC/stats.py
@@ -45,7 +45,7 @@ def jit_my_stuff():
 
         _mean_helper = jVMC.global_defs.pmap_for_my_devices(lambda data, w: jnp.tensordot(w, data, axes=(0,0)), in_axes=(0, 0))
         _var_helper = jVMC.global_defs.pmap_for_my_devices(lambda data, w: jax.vmap(lambda d, w: w * jnp.abs(d)**2, in_axes=(0,0))(data, w), in_axes=(0, 0))
-        _data_prep = jVMC.global_defs.pmap_for_my_devices(lambda data, w, mean: jax.vmap(lambda d, w, m: jnp.sqrt(w) * (d - m), in_axes=(0,0,None))(data, w, mean), in_axes=(0, 0, None))
+        _data_prep = jVMC.global_defs.pmap_for_my_devices(lambda data, w, mean: jax.vmap(lambda d, w, m: jnp.sqrt(w) * (d ), in_axes=(0,0,None))(data, w, mean), in_axes=(0, 0, None))
         _covar_helper = jVMC.global_defs.pmap_for_my_devices(
                                 lambda data1, data2:
                                     jnp.tensordot(

jVMC/util/tdvp.py
@@ -266,13 +266,8 @@ class TDVP:
         
         # Evaluate local energy
         start_timing(outp, "compute Eloc")
-        # TODO: add get_o_loc
         ElocObs = hamiltonian.get_o_loc(sampleConfigs, psi, samplePsi, t)
-        # print(Eloc/psi(sampleConfigs))
-        # Eloc = hamiltonian.get_O_loc(sampleConfigs, psi, sampleLogPsi, t)
         stop_timing(outp, "compute Eloc", waitFor=ElocObs)
-        # TODO: get_sampler_net call to obtain regularized p function
-        # TODO: apply the p function to all configurations
         ElocM = SampledObs(ElocObs, pMean)
         ElocV = SampledObs(ElocObs, pVar)
 
@@ -281,16 +276,16 @@ class TDVP:
         # Evaluate gradients
         start_timing(outp, "compute gradients")
         sampleGradientsObs = psi.gradients(sampleConfigs)
-        # print("sampleGradients: ", sampleGradients)
         stop_timing(outp, "compute gradients", waitFor=sampleGradientsObs)
-        # TODO: sort out how the regularization enters the gradiet computation
+
         sampleGradientsM = SampledObs( sampleGradientsObs, pMean)
         sampleGradientsV = SampledObs( sampleGradientsObs, pVar)
 
-        # print("grad: ", sampleGradientsM.mean(), ", ",  sampleGradientsV.var(), ", ", sampleGradientsV.var() - sampleGradientsM.mean()**2, ", ", addFact*(sampleGradientsV.var() - sampleGradientsM.mean()**2))
-
-        # print("F: ", addFact*(sampleGradientsV.covar(ElocV).ravel() - sampleGradientsM.mean()*ElocM.mean()))
-        # print("S: ", addFact*(sampleGradientsV.covar() - sampleGradientsM.mean()**2))
+        # print("Eloc: ", ElocM.mean(), ", ",  ElocV.var(), ", ", ElocV.var() - jnp.abs(ElocM.mean())**2)
+        # print("grad: ", sampleGradientsM.mean(), ", ",  sampleGradientsV.var(), ", ", sampleGradientsV.var() - jnp.abs(sampleGradientsM.mean())**2)
+        #
+        # print("F: ", (sampleGradientsV.covar(ElocV).ravel() - jnp.conj(sampleGradientsM.mean())*ElocM.mean()))
+        # print("S: ", (sampleGradientsV.covar() - jnp.abs(sampleGradientsM.mean())**2))
 
         start_timing(outp, "solve TDVP eqn.")
 